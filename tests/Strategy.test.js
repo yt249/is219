38149c6897c77bf1589589a9f55d69108f54cc34
yt249 <yt249@njit.edu>
2021-02-28 23:46:48 -0500

    finalize all tests

--- tests/Strategy.test.js
+++ tests/Strategy.test.js
@@ -5,3 +5,7 @@ test("Testing ConcreteStrategyAdd", () => {
     let result = new Strategy.ConcreteStrategyAdd;
     expect(result.execute(1,2)).toBe(3);
 });
+test("Testing ConcreteStrategySubtract", () => {
+    let result = new Strategy.ConcreteStrategySubtract();
+    expect(result.execute(1,2)).toBe(-1);
+});
